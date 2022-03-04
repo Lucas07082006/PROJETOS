36c4c7843d71dcd8717fbc5d343271928b114161
Lucas07082006 <99491738+Lucas07082006@users.noreply.github.com>
2022-03-04 12:21:01 -0300

    Create codigo.ino

---
+++ aula 1/codigo.ino
@@ -0,0 +1,16 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(2, INPUT);
+   pinMode(3, OUTPUT);
+}
+
+void loop()
+{
+  int  var  = digitalRead(2);
+   digitalWrite(3, var);
+  delay(10);
+  
+  }
+
